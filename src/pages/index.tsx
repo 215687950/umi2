c24b48e25c4ecf8c20d3bb563894ff24bfa126e0
jingy <jingy@woketech.com>
2022-06-08 10:51:11 +0800

    feat: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import styles from './index.less';
 
 export default function IndexPage() {
-  const a = {
-    b: 1,
-  };
-
-  if (a.b == 1) {
-    console.log(c);
-  }
-
   return (
     <div>
       <h1 className={styles.title}>
         Page indexPage indexPage indexPage indexPage indexPage indexPage indexindexPage111111111112
-        indexindexPage indexindexPage index
+        indexindexPage indexindexPage
       </h1>
     </div>
   );
